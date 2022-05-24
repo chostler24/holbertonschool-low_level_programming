60c32c068418617a8e3826b57d8e8af6f66500b8
chostler24 <connor.hostler@gmail.com>
2022-05-24 11:57:41 -0700

    absolute power

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: Always 0 (Success)
+ * @n: var
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
