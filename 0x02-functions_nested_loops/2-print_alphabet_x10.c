80639f943852f47ae5d32315663bacda44083eb9
chostler24 <connor.hostler@gmail.com>
2022-05-24 10:45:11 -0700

    fixed funct descript

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabetx10 - prints 10 times the alphabet, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet, followed by a new line
  * Return: Always 0 (Success)
  */
 
