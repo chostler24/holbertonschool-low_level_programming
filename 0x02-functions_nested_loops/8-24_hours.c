db8679226f7c5c840907de0f596b8953a1f0345d
chostler24 <connor.hostler@gmail.com>
2022-05-24 13:20:18 -0700

    jackie b

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,8 +4,22 @@
  * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
  * Return: Always 0 (Success)
- *
  */
 
 void jack_bauer(void)
 {
+int h = 0;
+int m = 0;
+while (h < 24)
+{
+_putchar((char)h / 10 + '0');
+_putchar((char)h % 10 + '0');
+_putchar('\n');
+}
+while (m < 59)
+{
+_putchar((char)m / 10 + '0');
+_putchar((char)m % 10 + '0');
+_putchar('\n');
+}
+}
