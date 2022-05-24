30cd248e7b67481deb6b02a9a38349d350667bac
chostler24 <connor.hostler@gmail.com>
2022-05-24 13:48:20 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * Return: result of addition
+ * @a: var
+ * @b: var
+ */
+
+int add(int a, int b)
+{
+int x;
+x = a + b;
+return (x);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ * @n: var
+ */
+
+void print_to_98(int n)
+{
