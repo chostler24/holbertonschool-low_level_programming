7b260e1eda8555ffb88faf3cdef586240f7ad291
chostler24 <connor.hostler@gmail.com>
2022-05-24 10:54:53 -0700

    fixed for betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 /**
  * _islower - checks for lowercase character
  * Return: 1 if lowercase, 0 otherwise
+ * @c : variable
  */
 
 int _islower(int c)
 {
-return (c >= 'a' && c <= 'z');
+return (c >= 97 && c <= 122);
 }
