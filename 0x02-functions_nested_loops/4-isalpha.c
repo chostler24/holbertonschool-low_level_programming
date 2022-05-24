006eee76e2148ddfe81a3d7eea465079275d0a3d
chostler24 <connor.hostler@gmail.com>
2022-05-24 11:25:03 -0700

    changed to if loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,10 @@
 
 int _isalpha(int c)
 {
-while (c >= 65 && c <= 90 && c >= 97 && c <= 122)
-{
+if (c >= 65 && c <= 90)
 return (1);
-}
+else if (c >= 97 && c <= 122)
+return (1);
+else
 return (0);
 }
