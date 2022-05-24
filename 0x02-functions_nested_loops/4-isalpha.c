37189080568bd01f5a2ca73b812cd62baf6d0dac
chostler24 <connor.hostler@gmail.com>
2022-05-24 11:11:50 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * @c: variable
+ */
+
+int _isalpha(int c)
+{
+while (c >= 65 && c >= 90 && c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
