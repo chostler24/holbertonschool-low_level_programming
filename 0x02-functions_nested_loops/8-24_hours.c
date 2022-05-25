7a0ebe601508e6d389d47291348a68754004e948
chostler24 <connor.hostler@gmail.com>
2022-05-25 08:33:55 -0700

    jackie b

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,16 @@ int h = 0;
 int m = 0;
 while (h < 24)
 {
-while (m < 60)
-{
-_putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
-_putchar(':');
-_putchar((m / 10) + '0');
-_putchar((m % 10) + '0');
-_putchar('\n');
-m++;
-}
-h++;
+	while (m < 60)
+	{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+		m++;
+	}
+	h++;
 }
 }
