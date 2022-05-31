936464bfbfb3819eb8780d77942b3ac3ce566c39
chostler24 <connor.hostler@gmail.com>
2022-05-31 13:16:20 -0700

    fixed brackets

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,8 +17,8 @@ void more_numbers(void)
 		{
 			if (n >= 10)
 			{
-				_putchar(n / 10 + 48);
-				_putchar(n % 10 + 48);
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
 			}
 		}
 		_putchar(10);
