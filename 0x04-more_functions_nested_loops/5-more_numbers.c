e2b3d5994b79a9db4fa5890b3b92746c893ec7de
chostler24 <connor.hostler@gmail.com>
2022-05-31 15:19:28 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n >= 10)
+			if (n > 9)
 			{
 				_putchar((n / 10) + 48);
 				_putchar((n % 10) + 48);
