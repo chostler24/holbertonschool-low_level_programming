5c7e5ba638cd322f34680f4d1b0b9735f693631e
chostler24 <connor.hostler@gmail.com>
2022-05-31 13:11:43 -0700

    fixed whitespace

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,12 +16,9 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
-			{
 				_putchar(n / 10 + 48);
 				_putchar(n % 10 + 48);
-			}
-
-			_putchar(10);
 		}
+		_putchar(10);
 	}
 }
