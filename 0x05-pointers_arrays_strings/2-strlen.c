0112253d47a052f255fcb03f5fc21d0e2ad749b0
chostler24 <connor.hostler@gmail.com>
2022-06-02 07:58:17 -0700

    added task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,17 @@
 
 /**
   * _strlen - function that returns the length of a string.
-  *
+  * @s: variable
   * Return: string length
   */
 
 int _strlen(char *s)
 {
+	int x = 0;
+
+	while (*s++)
+
+		x++;
+
+	return (x);
+}
