1e475af3cd3bcd2ad1b8b5c12c211d7d3addace7
chostler24 <connor.hostler@gmail.com>
2022-06-02 09:38:53 -0700

    added task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,9 +2,28 @@
 
 /**
   * rev_string - function that reverses a string.
-  *
+  * @s: variable
   * Return: none
   */
 
 void rev_string(char *s)
 {
+	int i = 0;
+	int j = 0;
+	char a[500];
+
+	while (s[i])
+	{
+		a[i] = s[i];
+		i++;
+	}
+
+	i = i - 1;
+
+	while (i >= 0)
+	{
+		s[i] = a[j];
+		j++;
+		i--;
+	}
+}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,9 +2,10 @@
 
 /**
   * puts_half - function that prints half of a string, followed by a new line.
-  *
+  * @str: string to be printed
   * Return: none
   */
 
 void puts_half(char *str)
 {
+
