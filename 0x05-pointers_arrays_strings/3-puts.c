6092f1d40e52c50e9b2ede1429e406fcce0295df
chostler24 <connor.hostler@gmail.com>
2022-06-02 08:06:26 -0700

    added task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,9 +3,18 @@
 /**
   * _puts - function that prints a string, followed by a new line,
   * to stdout.
-  *
+  * @str: variable
   * Return: none
   */
 
 void _puts(char *str)
 {
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar(10);
+
+}
