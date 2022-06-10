5ba9464f4c118164b6d6018b29f11d9e0bd8537c
chostler24 <connor.hostler@gmail.com>
2022-06-09 17:59:44 -0700

    added task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,35 @@
 #include "holberton.h"
 
 /**
-  * leet - function that encodes a string into 1337.
+  * *leet - function that encodes a string into 1337.
+  * @s: string
   * Return: char
   */
 
-char *leet(char *)
+char *leet(char *s)
 {
+	char *c1 = "aAeEoOtTlL";
+	char *c2 = "4433007711";
+	int i = 0;
+	int j;
 
+	while (s[i])
+	{
+		j = 0;
+
+		while (c1[j])
+		{
+			if (s[i] == c1[j])
+			{
+				s[i] = c2[j];
+			}
+
+			j++;
+		}
+
+		i++;
+
+	}
+
+	return (s);
 }
