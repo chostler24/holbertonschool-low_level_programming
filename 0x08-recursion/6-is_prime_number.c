4c264117699975f36125d679a94c41de6c72e805
chostler24 <connor.hostler@gmail.com>
2022-06-09 18:19:55 -0700

    added task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,8 +9,33 @@
 
 int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
+
+	return (prime_helper(n / 2, n));
+}
+
+
+/**
+  * prime_helper - returns 1 if prime, o otherwise
+  * @i: counter
+  * @n: integer
+  * Return: 1 if prime, 0 otherwise
+  */
+
+int prime_helper(int i, int n)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	return (prime_helper(i - 1, n));
 }
