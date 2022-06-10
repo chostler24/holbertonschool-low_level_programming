0f441e83d93587da2ba187d7acb209063303fb50
chostler24 <connor.hostler@gmail.com>
2022-06-09 17:42:00 -0700

    added task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,19 @@
   * Return: char
   */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+	int i = 0;
 
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+
+		i++;
+	}
+
+	return (s);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,5 +7,23 @@
 
 char *cap_string(char *)
 {
+	int i = 0;
 
-}
+	if (s[0] > 'a' && s[0] < 'z')
+	{
+		s[0] = s[0] - 32;
+	}
+
+	while (s[i])
+	{
+		switch (s[i - 1])
+		{
+			case ' ';
+			case '\+';
+			case ',';
+		}
+
+		if (s[i] > 'a' && s[i] < 'z')
+		{
+			s[i] = s[i] - 32;
+		}
