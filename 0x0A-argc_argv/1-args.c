1fc69ddbb00f07d657bd73ce474724ada4a234cf
chostler24 <connor.hostler@gmail.com>
2022-06-14 09:05:57 -0700

    added task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - function that prints the number of arguments passed to it.
+  * @argc: argument counter
+  * @argv: argument array
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
