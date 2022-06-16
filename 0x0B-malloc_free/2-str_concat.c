a9f48592ca7b5e0180cdba7eff39e74783bc9b38
chostler24 <connor.hostler@gmail.com>
2022-06-16 13:59:48 -0700

    Added task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * *str_concat - function that concatenates two strings.
+  * @s1: string 1
+  * @s2: string 2
+  * Return: NULL on failure
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	return (s1);
+}
