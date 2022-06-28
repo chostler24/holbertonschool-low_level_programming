16eb85fa47f90c4feafdb02e1a4bd6357bc9c58a
chostler24 <connor.hostler@gmail.com>
2022-06-28 14:12:06 -0700

    Added task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - function that prints numbers.
+  * @separator: char
+  * @n: integer
+  * Return: none
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int i;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+		printf("\n");
+	}
+
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%i\n", va_arg(args, int));
+		}
+
+		printf("\n");
+
+		va_end(args);
+	}
+
+	i = 1;
+
+	while (i < n)
+	{
+		printf("%i%s", va_arg(args, int), separator);
+		i++;
+	}
+
+	printf("%i\n", va_arg(args, int));
+
+	va_end(args);
+}
