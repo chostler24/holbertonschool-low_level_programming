307e27888f9a44c3143cb0b30d08028e331cf698
chostler24 <connor.hostler@gmail.com>
2022-06-28 14:19:17 -0700

    Fixed task 1 to include separator edge case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,12 +23,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 	{
+		separator = "";
+
 		for (i = 0; i < n; i++)
 		{
 			printf("%i\n", va_arg(args, int));
 		}
 
-		printf("\n");
 		va_end(args);
 		return;
 	}
