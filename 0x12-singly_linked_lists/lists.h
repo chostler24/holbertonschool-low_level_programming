992d909d5bedba955f2560e593f9a1c85283bfc2
chostler24 <connor.hostler@gmail.com>
2022-07-07 14:34:42 -0700

    Added header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
