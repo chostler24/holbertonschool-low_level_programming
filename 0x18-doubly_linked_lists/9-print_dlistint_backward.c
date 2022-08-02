0b39a09019d6da442b3bfa0675a604316a1303fc
chostler24 <connor.hostler@gmail.com>
2022-08-02 12:39:07 -0700

    Adding task 7/9

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  * print_dlistint_backward - prints all elements of a list backward
+  * @h: head of list pointer
+  * Return: nodes
+  */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	int nodes;
+
+	nodes = 0;
+
+	if (h == NULL)
+	{
+		return (nodes);
+	}
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+
+		nodes++;
+	}
+
+	return (nodes);
+}
