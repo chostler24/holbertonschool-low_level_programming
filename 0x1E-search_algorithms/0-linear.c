674bc8cc2fdeac6406bbd3f29bd59c55681feca1
chostler24 <connor.hostler@gmail.com>
2023-02-17 12:02:33 -0600

    Recompiled?

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        int x;
+        int x = 0;
 
         if (!array)
                 return (-1);
 
-        for (x = 0; x < (int) size; x++)
+        for (; x < (int)size; x++)
         {
                 printf("value checked array[%d] = [%d]\n", x, array[x]);
+
                 if (array[x] == value)
                         return (x);
         }
