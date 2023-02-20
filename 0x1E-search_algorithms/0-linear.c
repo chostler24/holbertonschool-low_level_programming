6312f6b3fee15581e30d83c4aa5b5fa7e52df888
chostler24 <connor.hostler@gmail.com>
2023-02-20 10:57:39 -0600

    Typo in printf statement

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 
         for (x = 0; x < (int)size; x++)
         {
-                printf("value checked array[%d] = [%d]\n", x, array[x]);
+                printf("Value checked array[%d] = [%d]\n", x, array[x]);
 
                 if (array[x] == value)
                         return (x);
